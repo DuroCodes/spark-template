aa79a2f5aa126c3ab7b2618dbf99efdf0b420070
DuroCodes <davidwright13503@gmail.com>
2023-03-30 18:36:47 -0400

    feat: initial commit

---
+++ src/commands/ping.ts
@@ -0,0 +1,9 @@
+import { SparkCommand, CommandType, Plugins } from '@spark.ts/handler';
+
+export default new SparkCommand({
+  type: CommandType.Slash,
+  plugins: [Plugins.Publish()],
+  run({ interaction }) {
+    interaction.reply('pong');
+  },
+});
